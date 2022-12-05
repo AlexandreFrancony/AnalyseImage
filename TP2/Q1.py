f6a7067517a95c7c241957e2c47ec4b35112a8c2
AlexandreFrancony <alexandre.francony@edu.devinci.fr>
2022-12-05 21:32:42 +0100

    Enoncé TP2_Q1&2

---
+++ TP2/Q1.py
@@ -0,0 +1,3 @@
+#Ecrire sous Python la fonction de convolution Convol(I,Mask) ou utiliser 
+#la fonction intégrée signal.convolve2d(I, Mask) de la librairie scipy de Python.
+#Interprétation des résultats de l'opérateur de renforcement de contraste. 
---
+++ TP2/Q2.py
@@ -0,0 +1,2 @@
+#Ecrire la fonction Derivation(I,Mask3x3) basée sur la convolution. Déterminer le masque 2D
+#de l'opérateur de dérivation (∇ 2D) Interprétation des résultats de l'opérateur de dérivation.

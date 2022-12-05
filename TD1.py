225bbb0ea35151bf8c7dcfe07385801e3d6ddc6d
Victor-Bonnin <victor.bonnin@edu.devinci.fr>
2022-12-05 21:26:33 +0100

    Q1

---
+++ TD1.py
@@ -0,0 +1,3 @@
+#Q1_1 : On change les pixels et donc lors du calcul des coordonnées de XR YR, ça donne des chiffres à virgule, sauf que dans les pixels, c'est des entiers.
+#       Donc il faut arrondir et donc il y aura des endroits qui ne seront jamais renseignés du fait de l'arrondi.
+#       

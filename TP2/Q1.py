db352c253d2c7945b08b9d3363ba5d37dc8b9a52
Alexandre FRANCONY <alexandre.francony@edu.devinci.fr>
2022-12-07 10:36:12 +0100

    Repub Joris

--- TP2/Q1.py
+++ TP2/Q1.py
@@ -2,24 +2,32 @@
 #la fonction intégrée signal.convolve2d(I, Mask) de la librairie scipy de Python.
 #Interprétation des résultats de l'opérateur de renforcement de contraste. 
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
 #Lecture de l'image
-I = plt.imread('images/coco.bmp')
+coco = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\Images\\coco.bmp"
+I = plt.imread(coco)
 
-#Masque de convolution
-Mask = np.array([[0,0,0],[0,1,0],[0,0,0]])
+#Masque de convolution de renforcement de contraste
+Mask = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
-#Convolution
-I2 = signal.convolve2d(I, Mask, boundary='symm', mode='same')
+#Convolution de l'image par le masque 1 fois
+M = signal.convolve2d(I, Mask, boundary='symm', mode='same')
+
+#Convolution de l'image par le masque 2 fois
+J = signal.convolve2d(M, Mask, boundary='symm', mode='same')
 
 #Affichage
 plt.figure()
 plt.imshow(I, cmap='gray')
 plt.title('Image originale')
 plt.figure()
-plt.imshow(I2, cmap='gray')
-plt.title('Image convoluee')
+plt.imshow(M, cmap='gray')
+plt.title('Image à contraste renforce')
+plt.figure()
+plt.imshow(J, cmap='gray')
+plt.title('Image à contraste renforce 2 fois')
 plt.show()
--- TP2/Q2.py
+++ TP2/Q2.py
@@ -1,2 +1,31 @@
 #Ecrire la fonction Derivation(I,Mask3x3) basée sur la convolution. Déterminer le masque 2D
 #de l'opérateur de dérivation (∇ 2D) Interprétation des résultats de l'opérateur de dérivation.
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+#Lecture de l'image
+cube = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\Images\\cube.bmp"
+I = plt.imread(cube)
+
+#Fonction Derivation(I,Mask3x3)
+def Derivation(I,Mask3x3):
+    return signal.convolve2d(I, Mask3x3, boundary='symm', mode='same')
+
+
+#Masque de convolution de dérivation
+Mask = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+
+#Convolution de l'image par le masque 1 fois
+D = Derivation(I, Mask)
+
+#Affichage
+plt.figure()
+plt.imshow(I, cmap='gray')
+plt.title('Image originale')
+plt.figure()
+plt.imshow(D, cmap='gray')
+plt.title('Image dérivée')
+plt.show()

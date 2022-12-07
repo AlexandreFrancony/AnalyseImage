737d716ff9ebad951648496eb3debfa24c0ee079
Alexandre FRANCONY <alexandre.francony@edu.devinci.fr>
2022-12-07 09:30:40 +0100

    Added folder "images" + Q2_1

--- TP2/Q1.py
+++ TP2/Q1.py
@@ -1,3 +1,25 @@
 #Ecrire sous Python la fonction de convolution Convol(I,Mask) ou utiliser 
 #la fonction intégrée signal.convolve2d(I, Mask) de la librairie scipy de Python.
 #Interprétation des résultats de l'opérateur de renforcement de contraste. 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+#Lecture de l'image
+I = plt.imread('images/coco.bmp')
+
+#Masque de convolution
+Mask = np.array([[0,0,0],[0,1,0],[0,0,0]])
+
+#Convolution
+I2 = signal.convolve2d(I, Mask, boundary='symm', mode='same')
+
+#Affichage
+plt.figure()
+plt.imshow(I, cmap='gray')
+plt.title('Image originale')
+plt.figure()
+plt.imshow(I2, cmap='gray')
+plt.title('Image convoluee')
+plt.show()

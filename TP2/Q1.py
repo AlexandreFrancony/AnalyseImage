a9306472b8e3feddde7ad0b6bbeef1f38a8bdf9e
Pierre VALLES <pierre.valles@edu.devinci.fr>
2022-12-10 17:59:18 +0100

    commit Q4

--- TP2/Q1.py
+++ TP2/Q1.py
@@ -11,23 +11,17 @@ from scipy import signal
 coco = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\Images\\coco.bmp"
 I = plt.imread(coco)
 
-#Masque de convolution de renforcement de contraste
-Mask = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+#Masque de convolution
+Mask = np.array([[0,0,0],[0,1,0],[0,0,0]])
 
-#Convolution de l'image par le masque 1 fois
-M = signal.convolve2d(I, Mask, boundary='symm', mode='same')
-
-#Convolution de l'image par le masque 2 fois
-J = signal.convolve2d(M, Mask, boundary='symm', mode='same')
+#Convolution
+I2 = signal.convolve2d(I, Mask, boundary='symm', mode='same')
 
 #Affichage
 plt.figure()
 plt.imshow(I, cmap='gray')
 plt.title('Image originale')
 plt.figure()
-plt.imshow(M, cmap='gray')
-plt.title('Image à contraste renforce')
-plt.figure()
-plt.imshow(J, cmap='gray')
-plt.title('Image à contraste renforce 2 fois')
+plt.imshow(I2, cmap='gray')
+plt.title('Image convoluee')
 plt.show()
